c53f26034ab706a70e6065384eb247979b133706
IgorZasypkin <zas.i.e@yandex.ru>
2020-11-04 19:12:51 +0300

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
+        int percent = 3;
+        int cash = 1000;
+        int deposit = percent * cash / 100;
+        System.out.println(deposit);
     }
 }
